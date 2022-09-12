81b22d1cbde7fbc344738d95b43fe6ad5667c18c
celil öztürk <celil.ozturk@windowslive.com>
2022-09-12 22:52:32 +0200

    Add project files.

---
+++ BookLibraryAppMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookLibraryAppMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
